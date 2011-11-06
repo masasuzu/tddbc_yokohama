80c09d472b94bc98dc4079f815ef5ac098f789b4
SUZUKI Masashi <m15.suzuki.masashi@gmail.com>
2011-11-07 00:28:26 +0900

    整形

--- perl/lib/BaseBallPlayer.pm
+++ perl/lib/BaseBallPlayer.pm
@@ -1,6 +1,7 @@
 package BaseBallPlayer;
 use strict;
 use warnings;
+use feature "switch";
 
 sub new {
     my ($class, $args) = @_;
@@ -14,22 +15,13 @@ sub batting_average {
         int ($self->{hit} / $self->{bat_at} * 10**3) / 10**3;
 }
 
-use feature "switch";
 sub format_string {
     my $self = $_[0];
     given ($self->batting_average) {
-        when (undef) {
-            return '----';
-        }
-        when (1.000) {
-            return '1.00';
-        }
-        when (0.00) {
-            return '.000';
-        }
-        default {
-            return sprintf '.%d', $self->batting_average * 1000 ;
-        }
+        when (undef) { return '----'; }
+        when (1.000) { return '1.00'; }
+        when (0.00)  { return '.000'; }
+        default      { return sprintf '.%d', $self->batting_average * 1000 ; }
     }
 }
 
